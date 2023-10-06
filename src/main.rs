f1e44b90c7ec8f71ea936c77a60776ae25b8ad39
Akihito Kirisaki <kirisaki@klara.works>
2023-10-06 21:04:59 +0900

    feat: plus numbers

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ impl<T> Stack<T> {
 }
 
 #[cfg(test)]
-mod tests {
+mod tests1 {
   use crate::Stack;
 
   #[test]
@@ -37,5 +37,52 @@ mod tests {
     stack.push(1);
     let _ = stack.pop();
     assert_eq!(stack.pop(), None)
+  }
+}
+
+struct Code (Vec<String>);
+
+impl Code {
+  fn parse(src :String) -> Self {
+    Code(src.split_ascii_whitespace().map(|x| {
+        x.to_string()
+    }).collect())
+  }
+
+  fn step(&mut self, stack :&mut Stack<u64>) {
+    let codes = self.0.clone();
+    let (head, rest) = codes.split_first().unwrap();
+    let op: &str = head;
+    self.0 = rest.to_vec();
+    match op {
+      "." => {
+        println!("{:}", stack.pop().unwrap())
+      },
+      "+" => {
+        let x = stack.pop().unwrap();
+        let y = stack.pop().unwrap();
+        stack.push(x + y);
+      }
+      num => {
+        let x = num.parse::<u64>().unwrap();
+        stack.push(x);
+      },
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests2 {
+  use crate::{Stack, Code};
+
+  #[test]
+  fn code_plus() {
+    let mut stack = Stack::<u64>::new();
+    let mut code = Code::parse("1 2 +".to_string());
+    code.step(&mut stack);
+    code.step(&mut stack);
+    code.step(&mut stack);
+    let actual = stack.pop().unwrap();
+    assert_eq!(actual, 3)
   }
 }
